46f5ae08dddc9e17f269755e388f409505bd3999
Gerardo Castillo <gerrcass@gmail.com>
2019-09-13 23:50:15 -0400

    Updates

---
+++ precache-manifest.d00d8fe4f7e9ab24b283730cc7785e96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc2f40796acfb7f4887dbf3a8316b45",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3d7a95132ab747b2d902",
+    "url": "/monsters-rolodex/static/css/main.520c6453.chunk.css"
+  },
+  {
+    "revision": "b899aa8eda48c935f7bc",
+    "url": "/monsters-rolodex/static/js/2.37d4496e.chunk.js"
+  },
+  {
+    "revision": "3d7a95132ab747b2d902",
+    "url": "/monsters-rolodex/static/js/main.94fbe715.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
